80f067c74b5e975cfbad859c3f9c032a6d8d152a
DavFilsDev <miharisoadavidfils@gmail.com>
2026-07-10 11:29:58 +0300

    refactor: send email

--- src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
@@ -1,48 +1,36 @@
 package com.example.demo.endpoint.rest.controller;
 
-import com.example.demo.mail.Email;
-import com.example.demo.mail.Mailer;
-import jakarta.mail.internet.InternetAddress;
+import com.example.demo.endpoint.event.consumer.SendEmailConfConsumer;
+import com.example.demo.endpoint.event.model.SendEmailRequested;
+import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.concurrent.CompletableFuture;
-
 @Slf4j
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
 
-    private final Mailer mailer;
+  private final SendEmailConfConsumer sendEmailConfConsumer;
 
-    @GetMapping("/hello")
-    public String helloWorld(
-            @RequestParam String to,
-            @RequestParam String title,
-            @RequestParam String content) {
+  @GetMapping("/hello")
+  public String helloWorld(
+      @RequestParam String to, @RequestParam String title, @RequestParam String content) {
 
-        CompletableFuture.runAsync(() -> {
-            try {
-                InternetAddress recipient = new InternetAddress(to);
-                Email email = new Email(
-                        recipient,
-                        Collections.emptyList(),
-                        Collections.emptyList(),
-                        "Sujet : " + title,
-                        "<html><body><h1>" + title + "</h1><p>" + content + "</p></body></html>",
-                        Collections.emptyList()
-                );
-                mailer.accept(email);
-                log.info("Email envoyé à {}", to);
-            } catch (Exception e) {
-                log.error("Échec de l'envoi à {}", to, e);
-            }
+    var event = SendEmailRequested.builder().to(to).pdfTitle(title).pdfContent(content).build();
+
+    CompletableFuture.runAsync(
+        () -> {
+          try {
+            sendEmailConfConsumer.accept(event);
+          } catch (Exception e) {
+            log.error("Error while sending email", e);
+          }
         });
 
-        return "... email sent successfully (without PDF)";
-    }
+    return "... pdf sent successfully";
+  }
 }
